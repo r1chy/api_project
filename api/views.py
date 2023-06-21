6ece2de1c36789d57092dc7fe11f88a52196d54e
Richard Miederer <richard.miederer@gmx.de>
2023-06-21 12:50:12 +0200

    added sh for subpreocess call

--- api/views.py
+++ api/views.py
@@ -2,11 +2,14 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
 import json
-
+import subprocess
 
 @api_view(['POST'])
 def getDatafromJson(request):
     data = json.loads(request.body)
     targeturl = data['url']
     #hier muss der curl passieren und damit ändert sich dann die response
-    return Response(targeturl)
+    #curl
+    rc = subprocess.check_call(['/home/rich/project/myproject/api/testcurl.sh', targeturl])
+    
+    return rc
